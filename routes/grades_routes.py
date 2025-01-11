85c684e37b37fed89e1ef83819ef5438679d5eee
Oktawik <oktawianpolak@gmail.com>
2025-01-11 12:22:21 +0100

    Zmiana tabelki na taka jaka chce wiktoria

--- routes/grades_routes.py
+++ routes/grades_routes.py
@@ -17,7 +17,6 @@ def grades():
 def gradesTeacher():
     return render_template('teacher/gradesTeacher.html', username=current_user.username, profession=current_user.profession)
 
-# Widok ocen ucznia
 @grades_blueprint.route('/gradesStudent')
 @login_required
 def gradesStudent():
